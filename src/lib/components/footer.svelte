6ba3297312e08dd1e9d92a8a6e25bd8864512c71
Leo Mangold <42837960+leo-mangold@users.noreply.github.com>
2023-01-05 13:12:26 +0100

    💄 UI: Footer col on mobile

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -4,19 +4,28 @@
 
 <footer>
 	<div class="mx-8 py-8 md:ml-auto md:w-2/3">
-		<div class="flex flex-row space-x-8">
+		<div class="flex flex-col gap-x-8 gap-y-8 sm:flex-row">
 			<div class="flex-1">
-				<div class="font-serif text-3xl font-bold">Leo Mangold</div>
+				<div class=" font-serif text-3xl font-bold ">Leo Mangold</div>
 			</div>
 			<div class="flex-1">
-				<a href="sms:{PHONE}" class="nav-link block"> Text </a>
-				<a href="mailto:{EMAIL}" class="nav-link block">Email</a>
+				<a href="sms:{PHONE}" class=" block">
+					<span class="underline-animation">Text me</span>
+				</a>
+
+				<a href="mailto:{EMAIL}" class=" block">
+					<span class="underline-animation">Email me</span>
+				</a>
 			</div>
 			<div class="flex-1">
 				<div>
 					<p>Delft, The Netherlands</p>
+					<p class="mt-2 text-xs">© Copyright 2023 Leo Mangold</p>
 				</div>
 			</div>
+		</div>
+		<div class="">
+			<!-- Copyright symbol -->
 		</div>
 	</div>
 </footer>
--- src/lib/components/nav.svelte
+++ src/lib/components/nav.svelte
@@ -4,12 +4,12 @@
 </script>
 
 <FadeIn>
-	<div class="navbar items-stretch space-y-2 p-8 sm:flex">
+	<div class="items-stretch space-y-2 p-8 sm:flex">
 		<a href="/" class=" flex-1">
 			<h2 class="font-serif text-4xl font-bold">Leo Mangold</h2>
 		</a>
-		<div class="nav-links my-auto flex-initial space-x-4">
-			<a href="sms:{PHONE}" class="underline-animation"> Text </a>
+		<div class="my-auto flex-initial space-x-4">
+			<a href="sms:{PHONE}" class="underline-animation">Text </a>
 			<a href="mailto:{EMAIL}" class="underline-animation">Email</a>
 			<!-- <a href="/contact" class="underline-animation">Contact</a> -->
 		</div>
